cd83616f1c2cb553c4e5c71837acbddb23f7480f
LE THE VIET <winviet10@gmail.com>
2021-07-23 00:13:34 +0700

    ver 1.0

--- server.js
+++ server.js
@@ -2,24 +2,28 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 
+const path = __dirname + '/app/views';
+
 const app = express();
-/*
+
+app.use(express.static(path));
+
 var corsOptions = {
   origin: "http://localhost:8081"
 };
-*/
 
-app.use(cors());
+
+app.use(cors(corsOptions));
 
 // parse requests of content-type - application/json
-//app.use(bodyParser.json());
+app.use(bodyParser.json());
 
 // parse requests of content-type - application/x-www-form-urlencoded
-//app.use(bodyParser.urlencoded({ extended: true }));
-
+app.use(bodyParser.urlencoded({ extended: true }));
+/*
 app.use(express.json({limit: '50mb'}));
 app.use(express.urlencoded({limit: '50mb'}));
-
+*/
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
@@ -27,6 +31,7 @@ app.use(function(req, res, next) {
 });
 
 const dp = require("./app/models");
+
 dp.mongoose
     .connect(dp.url, {
         useNewUrlParser: true,
@@ -44,6 +49,14 @@ dp.mongoose
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to Vietle application." });
 });
+
+
+//db.sequelize.sync();
+
+app.get('/', function (req, res){
+  res.sendFile(path + "index.html")
+})
+
 
 require("./app/routes/person.routes")(app);
 
